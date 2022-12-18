11bcf37df3ce60a61a179c95aafb21af5210c1d5
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-12-18 13:12:58 -0300

    feat: Component class and Component Funcional

--- primeiro-projeto/src/App.jsx
+++ primeiro-projeto/src/App.jsx
@@ -1,9 +1,24 @@
-function App() {
-  return (
-    <div>
-      <h1>Primeiro projeto</h1>
-    </div>
-  );
+/* Componente funcional */
+
+import { Component } from "react";
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>Componente Funcional</h1>
+//     </div>
+//   );
+// }
+
+/* Componente Class */
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Componente Class</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
