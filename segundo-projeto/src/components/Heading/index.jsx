aa3c08a646e22702a6284eb4ccf45091b6c9db24
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-12-19 23:08:25 -0300

    chore: Create secondary project used vitesp

--- segundo-projeto/src/components/Heading/index.jsx
+++ segundo-projeto/src/components/Heading/index.jsx
@@ -1,7 +1,12 @@
+import { node } from 'prop-types';
 import React from 'react';
 
 function Heading({ children }) {
   return <h1>{children}</h1>;
 }
+
+Heading.propTypes = {
+  children: node.isRequired,
+};
 
 export default Heading;
--- segundo-projeto/vite.config.js
+++ segundo-projeto/vite.config.js
@@ -1,19 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: "src",
-  build: {
-    outDir: "../dist",
-  },
   test: {
     globals: true,
-    environment: "jsdom",
-    include: ["**/(*.){test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    exclude: ["node_modules", "dist", ".idea", ".git", ".cache"],
-    setupFiles: "./tests/setupFiles.js",
+    environment: 'jsdom',
+    include: ['**/(*.){test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude: ['node_modules', 'dist', '.idea', '.git', '.cache'],
+    setupFiles: './tests/setupFiles.js',
     css: true,
   },
 });
