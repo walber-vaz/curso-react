704d2fc5cd5a11d269096fe307a865fd75a525a6
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-12-21 17:55:20 -0300

    chore: 📣 used Hooks - useContext Reac.Context

--- segundo-projeto/src/App.jsx
+++ segundo-projeto/src/App.jsx
@@ -1,71 +1,54 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
-import { shape, number, string, func } from 'prop-types';
+import { useContext, useState, createContext } from 'react';
 
-const Users = ({ post: { id, name, email, phone }, handleClick }) => {
-  console.log('Filho Renderizou');
+const globalState = {
+  title: 'React Hook = useContext',
+  body: 'useContext',
+  counter: 0,
+};
+
+const GlobalContext = createContext();
+
+// eslint-disable-next-line react/prop-types
+const Div = () => {
   return (
-    <div key={id}>
-      <h1 style={{ color: 'red' }} onClick={() => handleClick(name)}>
-        {name}
-      </h1>
-      <p>{email}</p>
-      <p>{phone}</p>
-    </div>
+    <>
+      <Heading />
+      <Content />
+    </>
   );
 };
 
-Users.propTypes = {
-  post: shape({
-    id: number,
-    name: string,
-    email: string,
-    phone: string,
-  }).isRequired,
-  handleClick: func,
+// eslint-disable-next-line react/prop-types
+const Heading = () => {
+  const {
+    contextState: { title },
+  } = useContext(GlobalContext);
+  return <h1>{title}</h1>;
+};
+
+// eslint-disable-next-line react/prop-types
+const Content = () => {
+  const {
+    contextState: { body, counter },
+    contextState,
+    setContextState,
+  } = useContext(GlobalContext);
+  return (
+    <p
+      onClick={() => setContextState({ ...contextState, counter: counter + 1 })}
+    >
+      {body} {counter}
+    </p>
+  );
 };
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [value, setValue] = useState('');
-  const inputRef = useRef(null);
-
-  console.log('Pai renderizou');
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch('https://jsonplaceholder.typicode.com/users')
-        .then((res) => res.json())
-        .then((res) => setUsers(res));
-    }, 500);
-  }, []);
-
-  useEffect(() => {
-    inputRef.current.focus();
-    console.log(inputRef.current);
-  }, [value]);
-
-  const handleClick = (value) => {
-    setValue(value);
-  };
+  const [contextState, setContextState] = useState(globalState);
 
   return (
-    <div>
-      <input
-        ref={inputRef}
-        type="search"
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-      />
-      {useMemo(() => {
-        return users.length <= 0 ? (
-          <h1>Carregando...</h1>
-        ) : (
-          users.map((post) => (
-            <Users key={post.id} post={post} handleClick={handleClick} />
-          ))
-        );
-      }, [users])}
-    </div>
+    <GlobalContext.Provider value={{ contextState, setContextState }}>
+      <Div />
+    </GlobalContext.Provider>
   );
 }
 
