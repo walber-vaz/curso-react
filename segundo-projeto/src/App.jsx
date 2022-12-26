eec14d809c4ae3782f7f7fa88783e3ed0b6c8482
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-12-26 09:47:26 -0300

    chore: custom Hooks

--- segundo-projeto/src/App.jsx
+++ segundo-projeto/src/App.jsx
@@ -1,45 +1,53 @@
-import { createContext, useContext, useReducer } from 'react';
-import { node } from 'prop-types';
+import React, { useEffect, useRef, useState } from 'react';
 
-// data.js
-const globalState = {
-  title: 'Hook useReducer',
-  body: 'useReducer',
-  counter: 0,
+const useMyHook = (callback, delay = 1000) => {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      savedCallback.current();
+    }, delay);
+
+    return () => clearInterval(interval);
+  }, [delay]);
 };
 
-// reducer.js
-export const reducer = (state, action) => {
-  return { ...state };
-};
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [delay, setDelay] = useState(1000);
+  const [increment, setIncrement] = useState(100);
 
-// appContext.js
-export const Context = createContext();
-export const AppContext = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, globalState);
+  useMyHook(() => setCounter(counter + 1), delay);
 
-  return <Context.Provider value={{ state }}>{children}</Context.Provider>;
-};
-
-AppContext.propTypes = {
-  children: node,
-};
-
-// Heading/index.js
-export const Heading = () => {
-  const context = useContext(Context);
-  return <h1>{context.state.title}</h1>;
-};
-
-// App.jsx
-function App() {
   return (
-    <AppContext>
-      <div>
-        <Heading />
-      </div>
-    </AppContext>
+    <>
+      <div>Contador: {counter}</div>
+      <div>Delay: {delay}</div>
+      <button
+        onClick={() => {
+          setDelay((d) => d + increment);
+        }}
+      >
+        +{increment}
+      </button>
+      <button
+        onClick={() => {
+          setDelay((d) => d - increment);
+        }}
+      >
+        -{increment}
+      </button>
+      <input
+        type="number"
+        value={increment}
+        onChange={(e) => setIncrement(parseInt(e.target.value))}
+      />
+    </>
   );
-}
+};
 
 export default App;
